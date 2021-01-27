ac521b7b28e35f2e541802d383b931a64e5fd375
darmis007 <darshmishra3010@gmail.com>
2021-01-27 16:57:12 +0530

    supplier added in models

--- BasantBookFestival/BookFest/models.py
+++ BasantBookFestival/BookFest/models.py
@@ -77,6 +77,7 @@ class Book(models.Model):
     description = models.CharField(max_length=2000, blank=True)
     subject = models.CharField(max_length=200, blank=True, null=True)
     image = models.URLField(blank=True, null=True)
+    supplier = models.CharField(max_length=500, blank=True,null=True)
     thumbnail = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
