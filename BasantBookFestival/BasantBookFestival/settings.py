2244a566a3bc82569dc5fd3c8ca1b4df0e5da993
darmis007 <darshmishra3010@gmail.com>
2021-02-01 13:03:23 +0530

    cors fix 1.1

--- BasantBookFestival/BasantBookFestival/settings.py
+++ BasantBookFestival/BasantBookFestival/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
     'rest_framework',
     'BookFest.apps.BookfestConfig',
-    "corsheaders",
 ]
 
 MIDDLEWARE = [
