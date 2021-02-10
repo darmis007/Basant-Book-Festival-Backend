a0edb1c09ad579647e80e5967fd02663867d46b8
darmis007 <darshmishra3010@gmail.com>
2021-02-10 14:27:44 +0530

    fix same site cookies 3

--- BasantBookFestival/BasantBookFestival/settings.py
+++ BasantBookFestival/BasantBookFestival/settings.py
@@ -44,8 +44,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django_cookies_samesite.middleware.CookiesSameSite',
+    'corsheaders.middleware.CorsMiddleware',   
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -148,6 +148,10 @@ USE_L10N = True
 USE_TZ = True
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+DCS_SESSION_COOKIE_SAMESITE = 'None'
+DCS_CSRF_COOKIE_SAMESITE = 'None'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
